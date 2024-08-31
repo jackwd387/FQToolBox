0c0165f9eb0cd2de566a4b6701430a3f29d4c809
jackwd387 <105554762+jackwd387@users.noreply.github.com>
2024-08-31 10:27:22 +0800

    Delete Test2.py

--- Test2.py
+++
@@ -1,9 +0,0 @@
-import requests,json,os
-def reading(tone_id,item_ids):
-  url = f"https://reading.snssdk.com/reading/reader/audio/playinfo/?tone_id={tone_id}&item_ids={item_ids}&aid=507386&version_code=999"
-  data = json.loads(requests.get(url=url).text)["data"][0]
-  print("主:"+data["main_url"])
-  print("备用:"+data["backup_url"])
-  return data["main_url"]
-os.system('mpv '+reading(input('音色:'),input('item_id:')))
-input('Enter键退出')
