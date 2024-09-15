d311d1078d5c58c4d9c9bddf641f66600fee6bab
jackwd387 <105554762+jackwd387@users.noreply.github.com>
2024-09-15 20:44:10 +0800

    Delete Main/Ui.py

--- Main/Ui.py
+++
@@ -1,8 +0,0 @@
-import wx
-app = wx.App() 
-def view_text(text):
-    window = wx.Frame(None, title = "Text", size = (300,200)) 
-    panel = wx.Panel(window) 
-    label = wx.StaticText(panel, label = text, pos = (0,0))
-    window.Show(True) 
-    app.MainLoop()
