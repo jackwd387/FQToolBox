f1c091da6cb3bfa0df5617bda90b4bf719fb1a4e
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2024-10-03 09:20:23 +0800

    Update fqtoolbox.sh

--- fqtoolbox.sh
+++ fqtoolbox.sh
@@ -1,10 +1,8 @@
 echo "请在下面的弹出的窗口点击允许"
 termux-setup-storage
-echo "修改为阿里镜像源，并更新软件包"
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.aliyun.com/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
-sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.aliyun.com/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
-sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.aliyun.com/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
-pkg update
+echo "修改镜像源，并更新软件包"
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list
+apt update && apt upgrade
 echo "安装python"
 pkg install python -y
 echo "安装依赖"
