7128171e63e57132bfc8f8c777b6f21970ee4753
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2024-10-07 19:30:05 +0800

    Update fqtoolbox.sh

--- fqtoolbox.sh
+++ fqtoolbox.sh
@@ -7,9 +7,11 @@ pkg update
 echo "安装python"
 pkg install python -y
 echo "python换源"
-pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple/
 echo "安装依赖"
-pip install requests edge-tts asyncio
+pip install requests
+pip install edge-tts
+pip install asyncio
 echo "从github获取FQToolBox，此处可能需要科学上网"
 curl -o FQToolBox.zip "https://codeload.github.com/jackwd387/FQToolBox/zip/refs/heads/main"
 unzip -d ~/storage/downloads FQToolBox.zip
