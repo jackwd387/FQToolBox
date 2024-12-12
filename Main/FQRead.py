eae3d8b30436111e405b2651b8aa977a61297f3c
Jackwd387 <105554762+jackwd387@users.noreply.github.com>
2024-12-12 23:50:57 +0800

    优化阅读书籍代码

--- Main/FQRead.py
+++ Main/FQRead.py
@@ -1,48 +1,42 @@
 import os
 import edge_tts
 import asyncio
-import _thread
+import threading
 from API import update_progres,user_bookshelf,book_id_inquire,item_id_inquire
 cookie = open('cookie.ini','r').read()
-executable = 'False'
 def thread(p):
-    global content,voice,rate_count,volume_count,executable
+    global content,voice,rate_count,volume_count
     print('正在爬取并生成音频')
-    if p > len(title_list):
-        None
+    if p+1 > len(title_list):
+        pass
     else:
-        content = item_id_inquire(item_id_list[p-1])[0]
-        print(f'文字数:{len(content)}')
-        asyncio.run(run_tts(title_list[p-1]+content,voice,rate_count,volume_count))
-        if executable == 'False':
-            executable = 'True'
+        content = item_id_inquire(item_id_list[p])[0]
+        #print(f'文字数:{len(content)}')
+        asyncio.run(run_tts(title_list[p]+content,voice,rate_count,volume_count))
 async def run_tts(text: str, voice: str,rate:str,volume:str) -> None:
     global title_list,output_files,count
     communicate =  edge_tts.Communicate(text=text, voice=voice,rate=rate,volume=volume)
-    await communicate.save(output_files+item_id_list[p-1+count]+'_TEMP.mp3')
-#   模拟浏览器
-headers = {
-    # User-Agent 用户代理, 表示浏览器/设备的基本身份信息
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
-    #cookie
-    ,'Cookie': cookie
-    }
-# url地址(小说主页)
+    await communicate.save(output_files+item_id_list[p]+'_TEMP.mp3')
 if __name__ == '__main__':
-    book_id = input('book_id(输入空则使用最近播放):')
+    user_data = user_bookshelf(cookie)
+    book_id = input('book_id(输入空则使用最近播放书籍):')
     if book_id == '':
-        book_id = user_bookshelf(cookie)[0][0]
+        book_id = user_data[0][0]
     data = book_id_inquire(book_id)
     title_list = data[1]
     item_id_list = data[0]
     name = data[2]
     for r in range(len(title_list)):
         print(f'章节 {r+1} :{title_list[r]}')
-    p = int(input('选择:'))
-    count = 0
+    p = input('选择(输入空则使用最近播放章节):')
     content = None
     output_files = './TEMP/' + book_id + '_ceche/'
-    os.system('edge-tts --list-voices')
+    if p == '':
+        item_id = user_data[1][0]
+        p = item_id_list.index(item_id)
+    else:
+        p = int(p) - 1
+    os.system('edge-tts  --list-voices')
     voice = input('请选择音色(默认zh-CN-XiaoxiaoNeural):')
     rate_count = input('语速大小(默认+0%):')
     volume_count = input('音量大小(默认+0%):')
@@ -54,20 +48,23 @@ if __name__ == '__main__':
         volume_count = '+0%' 
     if not os.path.exists(output_files):
         os.makedirs(output_files)
-    _thread.start_new_thread(thread,(p+count,))
+    initialize_thread = threading.Thread(target=thread,args=(p,))
+    initialize_thread.start()
     print('等待初始化完毕')
+    initialize_thread.join()
     while True:
-        if executable == 'True':
-            if len(title_list) <= p-1+count:
+            if len(title_list) <= p:
                 os.system(f'edge-playback --text  "章节播放完毕 感谢使用" --voice '+ voice)
                 break
             else:
-                executable = 'False'
-                title = title_list[p-1+count]
-                item_id = item_id_list[p-1+count]
-                count += 1
-                print('即将播放:'+name+':'+title)
-                _thread.start_new_thread(update_progres,(cookie,item_id))
-                _thread.start_new_thread(thread,(p+count,))
+                title = title_list[p]
+                item_id = item_id_list[p]
+                p += 1
+                print(f'即将播放:{name}:{title}')
+                update_progres_thread = threading.Thread(target=update_progres,args=(cookie,item_id))
+                update_progres_thread.start()
+                ready_thread = threading.Thread(target=thread,args=(p,))
+                ready_thread.start()
                 os.system('mpv '+'"'+output_files+item_id+'_TEMP.mp3'+'"')
                 os.remove(output_files+item_id+'_TEMP.mp3')
+                ready_thread.join()
